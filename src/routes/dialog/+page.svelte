a2c0da8556d164706571a307ed0f4cd2373f8597
Jordan Fannin <jfannin@ascend-innovations.com>
2023-09-26 10:20:50 -0400

    removed dialog

--- src/routes/dialog/+page.svelte
+++ src/routes/dialog/+page.svelte
@@ -1,18 +1,8 @@
 <script>
-	import { Button, Dialog, Page } from '$lib/index.js'
+	import { Page } from '$lib/index.js'
 
 </script>
 
 <Page>
 	<h1>Dialog</h1>
-	<Button callback={toggleDialog} text="Show Dialog" />
-	
 </Page>
-
-<style>
-	.test-1,
-	.test-2 {
-		border: 1px solid red;
-		width: fit-content;
-	}
-</style>
