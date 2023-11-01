8dc47e01daa4505abbbac3f66d70c63006416dba
Jordan Fannin <jfannin@ascend-innovations.com>
2023-11-01 11:45:32 -0400

    fixed redundant -subtle class suffix

--- src/lib/containers/tags/StatusTag.svelte
+++ src/lib/containers/tags/StatusTag.svelte
@@ -7,11 +7,11 @@
 	const statusKey = status ? status.replace(' ', '').toLowerCase() : ''
 
 	const statusColorMap = {
-		approved: 'success-subtle',
+		approved: 'success',
 		draft: 'neutral',
 		failed: 'error',
 		inprogress: 'warning',
-		pending: 'neutral-subtle',
+		pending: 'neutral',
 		scheduled: 'neutral',
 		sent: 'success',
 		subscribed: 'success',
