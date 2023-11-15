826c71df3af204ec8dfbce82ddb370a3279e7bed
Jordan Fannin <jfannin@ascend-innovations.com>
2023-11-15 10:00:52 -0500

    removed unused import

--- src/lib/components/containers/navbar/NavBarHeader.svelte
+++ src/lib/components/containers/navbar/NavBarHeader.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Button, NavBarToggleButton } from '$lib/index.js'
+	import { NavBarToggleButton } from '$lib/index.js'
 
 	export let closedHeaderComponent, openHeaderComponent, keepOpen, navBarOpen, toggleNavbar
 </script>
