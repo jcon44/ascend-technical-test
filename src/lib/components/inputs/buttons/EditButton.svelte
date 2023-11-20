831a979e43c8ccbdfe19f0b4c7f3e0451b33a2d9
Jordan Fannin <jfannin@ascend-innovations.com>
2023-11-20 10:47:07 -0500

    added url to edit button

--- src/lib/components/inputs/buttons/EditButton.svelte
+++ src/lib/components/inputs/buttons/EditButton.svelte
@@ -2,11 +2,13 @@
 	import { Button, Edit01SmallIcon } from '$lib/index.js'
 
 	export let callback,
-		classes = ['btn-m', 'btn-outline']
+		classes = ['btn-m', 'btn-outline'],
+		url
 </script>
 
 <Button
 	leftIcon={Edit01SmallIcon}
 	{callback}
 	{classes}
+	{url}
 />
