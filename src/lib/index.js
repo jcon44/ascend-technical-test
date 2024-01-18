7f335973a88d166efb484d363aae856a22380eb3
Jordan Fannin <jfannin@ascend-innovations.com>
2024-01-18 09:09:14 -0500

    v0.1.50, put AscendLoginBackground back in library

--- src/lib/index.js
+++ src/lib/index.js
@@ -55,6 +55,7 @@ export { default as XCircleExtraSmallIcon } from '$lib/assets/icons/xs/XCircleEx
 export { default as AscendLogo } from '$lib/assets/images/AscendLogo.svelte'
 export { default as AscendSymbol } from '$lib/assets/images/AscendSymbol.svelte'
 export { default as Error404Graphic } from '$lib/assets/images/Error404Graphic.svelte'
+export { default as AscendLoginBackground } from '$lib/assets/images/AscendLoginBackground.png'
 
 // Stylesheets
 export { default as GlobalCSS } from '$lib/stylesheets/global.css?inline'
