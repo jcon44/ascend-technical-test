028a797733a0ee5d692de0fd73a2fe62f378d7c7
Jordan Fannin <jfannin@ascend-innovations.com>
2024-01-25 14:23:36 -0500

    fixed typo

--- src/lib/utils/setStyles.js
+++ src/lib/utils/setStyles.js
@@ -1,5 +1,5 @@
 export default function setStyles(cssRules) {
 	for (const [name, value] of Object.entries(cssRules)) {
-		document.documentElemtn.style.setProperty(name, value)
+		document.documentElement.style.setProperty(name, value)
 	}
 }
