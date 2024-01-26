419e204e6bbed86a4c8a4623d402153393b56b50
Jordan Fannin <jfannin@ascend-innovations.com>
2024-01-25 20:09:48 -0500

    added isValidEmail

---
+++ src/lib/utils/isValidEmail.js
@@ -0,0 +1,8 @@
+export default function isValidEmail(email) {
+	if (
+		typeof email !== 'string' ||
+		email.length > 255
+	) return false
+	const emailRegex = /^.+@.+$/
+	return emailRegex.test(email)
+}
