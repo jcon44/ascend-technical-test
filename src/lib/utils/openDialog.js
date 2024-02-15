7b29a8ccf959c8682a3f38db6e0b7f523a134e6e
Jordan Fannin <jfannin@ascend-innovations.com>
2024-02-15 10:34:38 -0500

    simplified dialog

--- src/lib/utils/openDialog.js
+++ src/lib/utils/openDialog.js
@@ -1,10 +1,5 @@
-import { closeDialog } from '$lib/index.js'
-
 export default function openDialog(store, dialog) {
-	const preservedDialogData = store.dialog.data
-	store = closeDialog(store)
 	store.dialog.content = dialog
-	store.dialog.data = preservedDialogData
 	store.dialog.open = true
 	return store
 }
