5522e93d150b8ed6b9727b6d95d5c37327a17c83
Jordan Fannin <jfannin@ascend-innovations.com>
2024-02-15 10:29:50 -0500

    fixed dialog data

--- src/lib/utils/openDialog.js
+++ src/lib/utils/openDialog.js
@@ -1,8 +1,10 @@
 import { closeDialog } from '$lib/index.js'
 
 export default function openDialog(store, dialog) {
+	const preservedDialogData = store.dialog.data
 	store = closeDialog(store)
 	store.dialog.content = dialog
+	store.dialog.data = preservedDialogData
 	store.dialog.open = true
 	return store
 }
