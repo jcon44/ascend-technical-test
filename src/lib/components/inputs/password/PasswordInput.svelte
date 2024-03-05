e1e46d9d6232e32eb437dd9649116c8e35db0457
Jesse Hill <jhill@ascend-innovations.com>
2024-03-05 07:47:47 -0500

    added password input

---
+++ src/lib/components/inputs/password/PasswordInput.svelte
@@ -0,0 +1,71 @@
+<script>
+	import { Label, Tag } from '$lib/index.js'
+
+	export let autofocus = false,
+		description = '',
+		id = '',
+		label = '',
+		tag = null,
+		maxlength = '',
+		minlength = '',
+		name = '',
+		placeholder = '',
+		required = false,
+		styles = [],
+		tabindex = '',
+		password = ''
+</script>
+
+<div class="password-control">
+	<div class="password-title">
+		{#if label}
+			<Label
+				{id}
+				{label}
+			/>
+		{/if}
+		{#if tag !== null}
+			<Tag
+				content={tag?.content || ''}
+				icon={tag?.icon || null}
+				side={tag?.side || ''}
+				type={tag?.type || ''}
+			/>
+		{/if}
+	</div>
+	
+	{#if description}
+		<div class="body-xs">{description}</div>
+	{/if}
+	<!-- svelte-ignore a11y-autofocus -->
+	<input
+		bind:value={text}
+		class="password-input"
+		style={styles.join(';')}
+		type="password"
+		{autofocus}
+		{id}
+		{name}
+		{maxlength}
+		{minlength}
+		{placeholder}
+		{required}
+		{tabindex}
+	/>
+</div>
+
+<style>
+	.text-control {
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing03);
+		width: 100%;
+	}
+
+	.text-title {
+		display: flex;
+		gap: var(--spacing05);
+		justify-content: flex-start;
+		width: 100%;
+	}
+</style>
