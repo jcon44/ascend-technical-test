23c5e01f6c72d4b40dfc7f1e9b99504dfcccee47
Jordan Fannin <jfannin@ascend-innovations.com>
2024-03-10 22:09:11 -0400

    v0.1.84 footer bar update

--- src/lib/components/containers/footers/FooterBar.svelte
+++ src/lib/components/containers/footers/FooterBar.svelte
@@ -1,34 +1,38 @@
 <script>
-	import { ContactUsButton, PrivacyPolicyButton, TermsAndConditionsButton } from '$lib/index.js'
+	import { ContactUsButton, PageFooter, PrivacyPolicyButton, TermsAndConditionsButton } from '$lib/index.js'
 
 	export let contactUsURL, privacyPolicyURL, termsAndConditionsURL
 </script>
 
-<div class="footer-bar">
-	<div class="company-signature">
-		Powered by <strong>Ascend Innovations</strong>
+<PageFooter>
+	<div class="footer-bar">
+		<div class="company-signature">
+			Powered by <strong>Ascend Innovations</strong>
+		</div>
+		<div class="link-buttons">
+			{#if termsAndConditionsURL}
+				<TermsAndConditionsButton url={termsAndConditionsURL} />
+			{/if}
+			{#if privacyPolicyURL}
+				<PrivacyPolicyButton url={privacyPolicyURL} />
+			{/if}
+			{#if contactUsURL}
+				<ContactUsButton url={contactUsURL} />
+			{/if}
+		</div>
 	</div>
-	<div class="link-buttons">
-		{#if termsAndConditionsURL}
-			<TermsAndConditionsButton url={termsAndConditionsURL} />
-		{/if}
-		{#if privacyPolicyURL}
-			<PrivacyPolicyButton url={privacyPolicyURL} />
-		{/if}
-		{#if contactUsURL}
-			<ContactUsButton url={contactUsURL} />
-		{/if}
-	</div>
-</div>
+</PageFooter>
 
 <style>
 	.footer-bar {
 		align-items: center;
 		background-color: var(--background-base);
 		display: flex;
-		justify-content: space-between;
-		padding: 0 var(--spacing09);
 		height: var(--spacing13);
+		justify-content: space-between;
+		margin-top: auto;
+		padding: 0 var(--spacing09);
+		width: 100%;
 	}
 	.link-buttons {
 		display: flex;
