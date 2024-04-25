9278ab92711301c961a3b40efb0e3b3ad14ef0c5
Jordan Fannin <jfannin@ascend-innovations.com>
2024-04-24 22:38:03 -0400

    v0.1.107, added confirm button disabled prop, added subfolder for sort utils

--- src/lib/components/inputs/buttons/ConfirmButton.svelte
+++ src/lib/components/inputs/buttons/ConfirmButton.svelte
@@ -3,6 +3,7 @@
 
 	export let callback = null,
 		classes = ['btn-fit', 'btn-l', 'btn-square', 'btn-primary'],
+		disabled = false,
 		loading = false,
 		url = ''
 </script>
@@ -11,6 +12,7 @@
 	text="Confirm"
 	{callback}
 	{classes}
+	{disabled}
 	{loading}
 	{url}
 />
