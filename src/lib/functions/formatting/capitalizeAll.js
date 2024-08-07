4ebb900a7051cba899bfa18738cff675dbe729fc
Jordan Fannin <jfannin@ascend-innovations.com>
2024-08-07 17:20:53 -0400

    v0.1.193 capitalizeAll diagnostics

--- src/lib/functions/formatting/capitalizeAll.js
+++ src/lib/functions/formatting/capitalizeAll.js
@@ -1,13 +1,18 @@
 import { capitalize } from '$lib/index.js'
 
 export default function capitalizeAll(phrase) {
+	console.log('phrase = ', phrase)
 	const wordArray = phrase.split(' ')
+
+	console.log('wordArray = ', wordArray)
 
 	for (let i = 0; i < wordArray.length; i++) {
 		wordArray[i] = capitalize(wordArray[i])
+		console.log(`wordArray[${i}] = `, wordArray[i])
 	}
 
 	const capitalizedPhrase = wordArray.join(' ')
+	console.log('capitalizedPhrase = ', capitalizedPhrase)
 
 	return capitalizedPhrase
 }
