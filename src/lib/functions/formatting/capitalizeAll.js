61e2da444f9d36adf03626ab525612c3dc294cf6
Jordan Fannin <jfannin@ascend-innovations.com>
2024-08-07 17:07:25 -0400

    v0.1.191, fixed typo

--- src/lib/functions/formatting/capitalizeAll.js
+++ src/lib/functions/formatting/capitalizeAll.js
@@ -1,6 +1,6 @@
 import capitalize from '$lib/index.js'
 
-export default function captitalizeAll(phrase) {
+export default function capitalizeAll(phrase) {
 	const wordArray = phrase.split(' ')
 
 	for (let i = 0; i < wordArray.length; i++) {
