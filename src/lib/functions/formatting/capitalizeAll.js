383db718aa67eb14830a219c8756fc85e143b43e
Jordan Fannin <jfannin@ascend-innovations.com>
2024-08-07 17:03:37 -0400

    v0.1.190, fixed typo

--- src/lib/functions/formatting/capitalizeAll.js
+++ src/lib/functions/formatting/capitalizeAll.js
@@ -1,10 +1,10 @@
-import captitalize from '$lib/index.js'
+import capitalize from '$lib/index.js'
 
 export default function captitalizeAll(phrase) {
 	const wordArray = phrase.split(' ')
 
 	for (let i = 0; i < wordArray.length; i++) {
-		wordArray[i] = captitalize(wordArray[i])
+		wordArray[i] = capitalize(wordArray[i])
 	}
 
 	const capitalizedPhrase = wordArray.join(' ')
