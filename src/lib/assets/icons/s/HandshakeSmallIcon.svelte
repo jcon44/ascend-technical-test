64239e31605a5318dde19f32b6ba8507e4555cf1
Jesse Hill <jhill@ascend-innovations.com>
2024-09-17 09:58:21 -0400

    added new icons

---
+++ src/lib/assets/icons/s/HandshakeSmallIcon.svelte
@@ -0,0 +1,80 @@
+<script>
+    import { getGlobalColorPalette, Icon } from '$lib/index.js'
+	import { onMount } from 'svelte'
+
+	export let styles = [],
+        colorOverride
+
+	let palette = {}
+
+	onMount(() => (palette = getGlobalColorPalette()))
+</script>
+
+<Icon
+    size="24"
+    {styles}
+>
+    <path 
+        d="M11.724 4.99988L10.9111 4.18698C8.67441 1.95027 5.04797 1.95026 2.81126 4.18698V4.18698C0.574544 6.42369 0.574541 10.0501 2.81126 12.2868L3.69629 13.1719" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2"
+    />
+    <path 
+        d="M10.5722 3.86132L13.3076 6.59669C14.1296 7.41865 14.1296 8.75129 13.3076 9.57325V9.57325C12.4857 10.3952 11.153 10.3952 10.3311 9.57325L8.31162 7.55381" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2" 
+        stroke-linecap="round"
+    />
+    <path 
+        d="M11.8396 5.0002L12.6525 4.1873C14.8892 1.95059 18.5156 1.95059 20.7524 4.1873C22.8297 6.26463 22.9777 9.54067 21.1965 11.7888" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2"
+    />
+    <path 
+        d="M8.63871 8.62666L3.72821 13.5372C2.99291 14.2725 2.99292 15.4646 3.72821 16.1999V16.1999C4.4635 16.9352 5.65564 16.9352 6.39093 16.1999L9.72883 12.862" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2" 
+        stroke-linecap="round"
+    />
+    <path 
+        d="M9.83404 12.7634L6.84149 15.7559C6.1062 16.4912 6.1062 17.6833 6.84149 18.4186V18.4186C7.57678 19.1539 8.76892 19.1539 9.50421 18.4186L11.7638 16.159" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2" 
+        stroke-linecap="round"
+    />
+    <path 
+        d="M14.3899 13.5295L10.9135 17.0059C10.1782 17.7412 10.1782 18.9333 10.9135 19.6686V19.6686C11.6488 20.4039 12.8409 20.4039 13.5762 19.6686L14.5328 18.7121" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2" 
+        stroke-linecap="round"
+    />
+    <rect 
+        x="14" y="18.2207" 
+        width="3.40395" 
+        height="3.40395" 
+        rx="1.70197" 
+        transform="rotate(-45 14 18.2207)" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2"
+    />
+    <rect 
+        x="16.4072" 
+        y="15.814" 
+        width="3.40395" 
+        height="3.40395" 
+        rx="1.70197" 
+        transform="rotate(-45 16.4072 15.814)" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2"
+    />
+    <rect 
+        x="18.814" 
+        y="13.4072"
+        width="3.40395" 
+        height="3.40395" 
+        rx="1.70197" 
+        transform="rotate(-45 18.814 13.4072)" 
+        stroke={colorOverride || "#6F6697"} 
+        stroke-width="2"
+    />
+</Icon>
---
+++ src/lib/assets/icons/s/MentalHealthSmallIcon.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { getGlobalColorPalette, Icon } from '$lib/index.js'
+	import { onMount } from 'svelte'
+
+	export let styles = [],
+        colorOverride
+
+	let palette = {}
+
+	onMount(() => (palette = getGlobalColorPalette()))
+</script>
+
+<Icon
+	size="24"
+	{styles}
+>
+    <g clip-path="url(#clip0_3012_369)">
+        <path 
+            d="M4 12L1.64138 15.931C1.31223 16.4796 1.57573 17.1919 2.18264 17.3942L4 18V21C4 21.5523 4.44772 22 5 22H7C7.55228 22 8 22.4477 8 23V23" 
+            stroke={colorOverride || "#6F6697"} 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+        />
+        <path 
+            d="M4 12V11C4 6.02944 8.02944 2 13 2C17.9706 2 22 6.02944 22 11C22 14.5031 20.8671 16.6587 19.0436 18.8264C18.3938 19.5989 18 20.561 18 21.5704V23" 
+            stroke={colorOverride || "#6F6697"} 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+        />
+        <path 
+            fill-rule="evenodd" 
+            clip-rule="evenodd" 
+            d="M19 1H15V5L11 5V9H15V13H19V9H23V5L19 5V1Z" 
+            fill="white"
+        />
+        <path 
+            d="M15 1V0C14.4477 0 14 0.447715 14 1L15 1ZM19 1H20C20 0.447715 19.5523 0 19 0V1ZM15 5V6C15.2652 6 15.5196 5.89464 15.7071 5.70711C15.8946 5.51957 16 5.26522 16 5H15ZM11 5L11 4C10.7348 4 10.4804 4.10536 10.2929 4.29289C10.1054 4.48043 10 4.73478 10 5H11ZM11 9H10C10 9.55229 10.4477 10 11 10V9ZM15 9H16C16 8.44772 15.5523 8 15 8V9ZM15 13H14C14 13.5523 14.4477 14 15 14V13ZM19 13V14C19.5523 14 20 13.5523 20 13H19ZM19 9V8C18.4477 8 18 8.44772 18 9H19ZM23 9V10C23.5523 10 24 9.55229 24 9H23ZM23 5H24C24 4.44772 23.5523 4 23 4V5ZM19 5H18C18 5.55228 18.4477 6 19 6V5ZM15 2H19V0H15V2ZM16 5V1H14V5H16ZM11 6H15V4L11 4L11 6ZM12 9V5H10V9H12ZM15 8H11V10H15V8ZM16 13V9H14V13H16ZM19 12H15V14H19V12ZM18 9V13H20V9H18ZM23 8H19V10H23V8ZM22 5V9H24V5H22ZM19 6L23 6V4L19 4V6ZM18 1V5H20V1H18Z" 
+            fill={colorOverride || "#6F6697"}
+        />
+    </g>
+    <defs>
+        <clipPath id="clip0_3012_369">
+            <rect width="24" height="24" fill="white"/>
+        </clipPath>
+    </defs>
+</Icon>
