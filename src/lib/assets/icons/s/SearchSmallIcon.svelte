c41fcfb36097c5cf3e37f708847a7d9ece0e17af
Jesse Hill <jhill@ascend-innovations.com>
2024-09-17 15:00:39 -0400

    updated color override for search icon

--- src/lib/assets/icons/s/SearchSmallIcon.svelte
+++ src/lib/assets/icons/s/SearchSmallIcon.svelte
@@ -2,7 +2,8 @@
 	import { getGlobalColorPalette, Icon } from '$lib/index.js'
 	import { onMount } from 'svelte'
 
-	export let styles = []
+	export let styles = [],
+		colorOverride
 
 	let palette = {}
 
@@ -15,7 +16,7 @@
 >
 	<path
 		d="M21 21L15.0001 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-		stroke={palette.neutral}
+		stroke={colorOverride || palette.neutral}
 		stroke-width="2"
 		stroke-linecap="round"
 		stroke-linejoin="round"
