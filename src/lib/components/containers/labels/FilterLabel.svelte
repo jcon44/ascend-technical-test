c94f1ae520320e48369080830ce30450b6915139
Jesse Hill <jhill@ascend-innovations.com>
2025-01-27 12:29:09 -0500

    truncated filter labels for ease of reading

--- src/lib/components/containers/labels/FilterLabel.svelte
+++ src/lib/components/containers/labels/FilterLabel.svelte
@@ -8,7 +8,7 @@
 <TooltipElement>
     <div slot="element" class="filter-label">
         <svelte:component this={filter.icon} />
-        <p class="body-bold-xs">{text}</p>
+        <p class="body-bold-xs text">{text}</p>
     </div>
     <div slot="content">
         {filter.tooltip}
@@ -21,5 +21,11 @@
         align-items: center;
         gap: var(--spacing03);
         color: var(--neutral-400);
+    }
+    .text{
+        max-width: 300px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
     }
 </style>
